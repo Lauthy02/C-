f3c2218c5f95acd10be974835e91a27792c083b8
Lautaro Rojas <lautaro.rojas02@gmail.com>
2022-08-30 17:35:15 -0300

    Agregue todos los ejercicios restantes

--- TPs-Prog1/TP1/TP1-PracticosEnConsola/Ejercicio15/Program.cs
+++ TPs-Prog1/TP1/TP1-PracticosEnConsola/Ejercicio15/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ejercicio10
+namespace Ejercicio15
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Dada una frase cualquiera llamada x, mostrar como resultado una frase
-            //formada por la segunda mitad de x más la primer mitad de x.
-            //(usar el método substring)
 
         }
     }
